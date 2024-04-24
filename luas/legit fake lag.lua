7bf3dfce8bab8f43fb4912f94accd09185bb12a3
bucket <155188548+busket132@users.noreply.github.com>
2024-04-24 19:13:02 +0300

    Update legit fake lag.lua

--- luas/legit fake lag.lua
+++ luas/legit fake lag.lua
@@ -1,3 +1,4 @@
+Enabled = 0
 FakeLagInterval = 250  -- time between fake lag enables
 FakeLagTime = 250  -- time that allow fake lag to be enabled
 InfoPrintEnabled = 0
@@ -10,49 +11,56 @@ local localPlayer = entities.GetLocalPlayer()
 Random1 = 0
 Random2 = 0
 
-engine.Notification('Fake lag timer info', 'To print info write lua InfoPrintEnabled = 1, to disable set 0 \nTo change time between info is printed write lua InfoPrintInterval = YOUR VALUE \nTo change time between fake lag enable write lua FakeLagInterval = YOUR VALUE \nTo change time that fake lag enabled write lua FakeLagTime = YOUR VALUE \nTo add a bit random write lua Randomnes = 1 \nFor your value 100 = 1 second')
+engine.Notification('Fake lag timer info', 'To enable legit fake lag write lua Enabled = 1 in console \nTo print info write lua InfoPrintEnabled = 1, to disable set 0 \nTo change time between info is printed write lua InfoPrintInterval = YOUR VALUE \nTo change time between fake lag enable write lua FakeLagInterval = YOUR VALUE \nTo change time that fake lag enabled write lua FakeLagTime = YOUR VALUE \nTo add a bit random write lua Randomnes = 1 \nFor your value 100 = 1 second')
 
 callbacks.Register("CreateMove", function ()
     if localPlayer:IsAlive() then
-        if FakeLagInterval + Random1 <= FakeLagIntervalTime then
-            FakeLagTimeOn = FakeLagTimeOn + 1
-            if FakeLagTime  >= FakeLagTimeOn then
-                gui.SetValue('Fake Lag', 1)
-                gui.SetValue('Fake Latency', 1)
-                gui.SetValue('Fake Latency Value (MS)', 500)
-                gui.SetValue('BackTrack', 1)
-            elseif FakeLagTime + Random2 <= FakeLagTimeOn then
-                FakeLagTimeOn = 0
-                FakeLagIntervalTime = 0
-                if Randomnes == 1 then
-                    Random1 = math.random(FakeLagInterval*-1 + 15, FakeLagInterval/2.5)
-                    Random2 = math.random(FakeLagTime*-1 + 15, FakeLagTime/2.5)
-                else
-                    Random1 = 0
-                    Random2 = 0
+        if Enabled == 1 then
+            if FakeLagInterval + Random1 <= FakeLagIntervalTime then
+                FakeLagTimeOn = FakeLagTimeOn + 1
+                if FakeLagTime  >= FakeLagTimeOn then
+                    gui.SetValue('Fake Lag', 1)
+                    gui.SetValue('Fake Latency', 1)
+                    gui.SetValue('Fake Latency Value (MS)', 500)
+                    gui.SetValue('BackTrack', 1)
+                elseif FakeLagTime + Random2 <= FakeLagTimeOn then
+                    FakeLagTimeOn = 0
+                    FakeLagIntervalTime = 0
+                    if Randomnes == 1 then
+                        Random1 = math.random(FakeLagInterval*-1 + 15, FakeLagInterval/2.5)
+                        Random2 = math.random(FakeLagTime*-1 + 15, FakeLagTime/2.5)
+                    else
+                        Random1 = 0
+                        Random2 = 0
+                    end
                 end
+            else
+                gui.SetValue('Fake Lag', 0)
+                gui.SetValue('Fake Latency', 0)
+                gui.SetValue('Fake Latency Value (MS)', 50)
+                gui.SetValue('BackTrack', 0)
+                FakeLagIntervalTime = FakeLagIntervalTime + 1
             end
-        else
+            if InfoPrintEnabled == 1 then
+                if InfoPrintInterval == InfoPrinted then
+                --print('Fake',FakeLagIntervalTime)
+                --print(FakeLagTimeOn)
+                print('FakeLagInterval =', FakeLagInterval + Random1)
+                print('FakeLagTime =' ,FakeLagTime + Random2)
+                print('Randomnes =', Randomnes)
+                InfoPrinted = 0
+                elseif InfoPrinted < InfoPrintInterval then
+                    InfoPrinted = InfoPrinted + 1
+                elseif InfoPrinted > InfoPrintInterval then
+                    InfoPrinted = 0
+                end
+            else
+            end
+        else 
             gui.SetValue('Fake Lag', 0)
             gui.SetValue('Fake Latency', 0)
             gui.SetValue('Fake Latency Value (MS)', 50)
             gui.SetValue('BackTrack', 0)
-            FakeLagIntervalTime = FakeLagIntervalTime + 1
-        end
-        if InfoPrintEnabled == 1 then
-            if InfoPrintInterval == InfoPrinted then
-            --print('Fake',FakeLagIntervalTime)
-            --print(FakeLagTimeOn)
-            print('FakeLagInterval =', FakeLagInterval + Random1)
-            print('FakeLagTime =' ,FakeLagTime + Random2)
-            print('Randomnes =', Randomnes)
-            InfoPrinted = 0
-            elseif InfoPrinted < InfoPrintInterval then
-                InfoPrinted = InfoPrinted + 1
-            elseif InfoPrinted > InfoPrintInterval then
-                InfoPrinted = 0
-            end
-        else
         end
     else
         gui.SetValue('Fake Lag', 0)
